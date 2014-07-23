b769cf27ee4772756c3c85476dc3e44e15b861d2
Aleksandr <andreev-a@open.ru>
2014-07-23 19:02:01 +0400

    edit feature

--- features/step_definitions/lead_steps.rb
+++ features/step_definitions/lead_steps.rb
@@ -3,12 +3,12 @@
   visit new_lead_url
 end
 
-Если(/^пользователь отправляет невалидную информацию о заявке$/) do
-  pending # express the regexp above with the code you wish you had
+Если(/^пользователь отправляет не всю информацию о заявке$/) do
+  click_button 'Сохранить'
 end
 
-То(/^он должен видеть сообщение об ошибке$/) do
-  pending # express the regexp above with the code you wish you had
+То(/^он должен видеть сообщение (.*)$/) do |message|
+  expect(page).to have_content(message)
 end
 
 Если(/^пользователь отправляет валидную информацию о заявке$/) do
