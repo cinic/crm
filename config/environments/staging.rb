1e44c9957c05911fd95df7151f665bbc770f1c4c
Alexander Andreev <cinic.rus@gmail.com>
2014-07-28 20:28:28 +0400

    Change app name

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -1,4 +1,4 @@
-Toplast::Application.configure do
+Crm::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
