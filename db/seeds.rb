516d20879eb25ebcfee363e160f41edd7c80af2b
Alexander Andreev <cinic.rus@gmail.com>
2014-08-04 16:49:14 +0400

    Add cyrillic support

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,2 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+#encoding: utf-8
 User.create([{name: 'Evgeny Savin', email: 'savinru@gmail.com', password: 'password',  role: 0}, {name: 'Alexander Andreev', email: 'cinic.rus@gmail.com', password: 'password',  role: 0}, {name: 'Test Test', email: 'test@test.com', password: 'password',  role: 1}, {name: 'Тихонов Сергей Сергеевич', email: 'test101@test.com', password: 'password',  role: 2}])
