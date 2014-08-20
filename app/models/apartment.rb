68c7420576759e9a4da804090e57f59ae8c084f0
Alexander Andreev <cinic.rus@gmail.com>
2014-08-20 04:07:24 +0400

    Change path for photos

--- app/models/apartment.rb
+++ app/models/apartment.rb
@@ -2,7 +2,7 @@ class Apartment < ActiveRecord::Base
   has_many :deals
   belongs_to :landlord
   belongs_to :user
-  has_attached_file :photo, :styles => { :large => "800>", :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/photo/:style/missing.png"
+  has_attached_file :photo, :styles => { :large => "800>", :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/photo/:style/missing.png", :url => "/upload/:class/:attachment/:id_partition/:style/:filename"
   
   validates_attachment_content_type :photo, :content_type => /\Aimage\/.*\Z/
   validates_presence_of :address
